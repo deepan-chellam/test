eeaa320fef15f5c7fb944c30ecf815cc7a039b84
deepan-chellam <zyzfdaalsjf@gmail.com>
2022-05-02 16:58:36 +0530

    changign the logic usning python inbuild dict method - get()

--- assignment_2_python/decode_nestedobject.py
+++ assignment_2_python/decode_nestedobject.py
@@ -1,40 +1,36 @@
 
 
+# function will decode the nested dict passed with keys and return the final value
+def decode_dict(nested_dictobj, keys):
+    orig_dict = nested_dictobj
+    mylist_keys = keys.split('/')
 
-def decode (objv, keyv):
-    #first getting the key values in list data type
-    kvlist = keyv.split('/')
-
-    #just of information
-    print("after conversino kvlist =".format(kvlist))
-
-    # looping the keys, until we reach the final key
-    for k in kvlist:
-        objv=objv[k]
-
-    # once we exit from for loop, we will get the final value in "objv" object
-    print( objv )
+    # try block to catch if passed nested dict and keys is out of sync.
+    try:
+        for key in mylist_keys:
+            nested_dictobj=nested_dictobj.get(key)
+    except:
+        print("something went wrong, while decoding the nested dict. kindly check input nested_dict and keys are proper")
 
 
-#Test3
+    print("final value of {0} is ====> {1}".format(orig_dict,nested_dictobj) )
+
+#Test1
 obj1 = {"a":{"b":{"c":"d"}}}
 key1="a/b/c"
-decode(obj1, key1)
+decode_dict(obj1, key1)
 
 #Test2
 obj2 = {"x":{"y":{"z":"a"}}}
 key2 = "x/y/z"
-decode(obj2, key2)
+decode_dict(obj2, key2)
 
 #Test3
 obj2 = {"x1":{"x2":{"x3":{"x4":{"x5":{"x6":{"x7":"final_value"}}}}}}}
 key2 = "x1/x2/x3/x4/x5/x6/x7"
-decode(obj2, key2)
+decode_dict(obj2, key2)
 
-# Expected output
-# after conversino kvlist =
-# d
-# after conversino kvlist =
-# a
-# after conversino kvlist =
-# 'final_value'
+# Expected output for the nested object passed
+# final value of {'a': {'b': {'c': 'd'}}} is ====> d
+# final value of {'x': {'y': {'z': 'a'}}} is ====> a
+# final value of {'x1': {'x2': {'x3': {'x4': {'x5': {'x6': {'x7': 'final_value'}}}}}}} is ====> final_value
