515e4f1cf4e4eaa0e6507650bbde4311ebabb0dc
deepan-chellam <zyzfdaalsjf@gmail.com>
2022-05-03 23:33:19 +0530

    enchanced the python script to be more configurable- to get ec2 instane meta data

--- assignment3_python/metadata-ec2.py
+++ assignment3_python/metadata-ec2.py
@@ -1,18 +1,61 @@
 
 import boto3
-import json
-management_console_obj = boto3.session.Session(profile_name='stage')
-ec2_client_obj = management_console_obj.client( region_name="us-west-2", service_name="ec2")
-
-input_instanceid=str(input("Kindly enter the instanceid for which you need meta data = "))
-ec2_userdata_json = ec2_client_obj.describe_instance_attribute(Attribute= 'userData', DryRun=False, InstanceId=input_instanceid)
-
-print(ec2_userdata_json)
+import pprint
 
 
-#########Expected output
-# C:\Users\chellamu\AppData\Local\Programs\I-Installed\Python\Python310\python.exe C:/Users/chellamu/Desktop/studycircle_2022/labsetup/kpmg-terraform/metadata-ec2.py
-# Kindly enter the instanceid for which you need meta data = i-00ae74139dc41a36d
-# {'InstanceId': 'i-00ae74139dc41a36d', 'UserData': {'Value': 'IyEvYmluL2Jhc2gKeXVtIHVwZGF0ZSAteQp5dW0gaW5zdGFsbCAteSBodHRwZApzeXN0ZW1jdGwgc3RhcnQgaHR0cGQKc3lzdGVtY3RsIGVuYWJsZSBodHRwZAplY2hvICI8aDE+SGVsbG8gd29ybGQgZnJvbSAkKGhvc3RuYW1lIC1mKTwvaDE+IiA+IC92YXIvd3d3L2h0bWwvaW5kZXguaHRtbA=='}, 'ResponseMetadata': {'RequestId': 'ef39d8e1-badb-4e98-9979-5d59772ced27', 'HTTPStatusCode': 200, 'HTTPHeaders': {'x-amzn-requestid': 'ef39d8e1-badb-4e98-9979-5d59772ced27', 'cache-control': 'no-cache, no-store', 'strict-transport-security': 'max-age=31536000; includeSubDomains', 'content-type': 'text/xml;charset=UTF-8', 'content-length': '552', 'date': 'Sun, 01 May 2022 23:55:39 GMT', 'server': 'AmazonEC2'}, 'RetryAttempts': 0}}
-#
-# Process finished with exit code 0
+def get_instance_metadata(input_region_name, input_instanceid=None):
+    #prerequiste stage profile is configured locally before running this script
+    try:
+        management_console_obj = boto3.session.Session(profile_name='stage')
+        ec2_client_obj = management_console_obj.client( region_name=input_region_name, service_name="ec2")
+
+
+        if len(input_instanceid)>0:
+            # planning to retrive meta data for list of ec2 instances given in user input
+            ec2_userdata_json = ec2_client_obj.describe_instances(InstanceIds=input_instanceid, DryRun=False, )
+        else:
+            #retrieve all the ec2 instances
+            ec2_userdata_json = ec2_client_obj.describe_instances(DryRun=False)
+
+    except Exception as e:
+        print("Somethning went wrong while getting meta data of ec2 with {0}".format(e))
+    pprint.pprint(ec2_userdata_json)
+
+
+#prerequiste using "aws configure" kindly configure the AWS_ACCESS_KEY and AWS_SECRET_ACCESS_KEY with prfile
+# ex: <aws configure --profile stage>
+# main program starts here
+
+print(
+'''
+PREREQUISTE:
+    using "aws configure" kindly configure the AWS_ACCESS_KEY and AWS_SECRET_ACCESS_KEY with profile
+    ex: <aws configure --profile stage>
+'''
+)
+# collecting the region name . Ex: us-west-2
+input_region_name=str(input("kindly enter the region name = "))
+
+
+flag=True
+#get the list of instance id's
+input_instance_list=[]
+
+#looping to collect the list of instance id's for which meta data retrieved.
+# if use want's to collect meta data to all the instances, then need to press "all"
+while flag:
+    input_instanceid = str(input("kindly enter either of below input below values\n 1. ec2-instanceid ==> if you have the list  \n 2. all ==> to collect all existing ec2 instances \n  "))
+    if input_instanceid == "all":
+        flag=False
+    else:
+        input_instance_list.append(input_instanceid)
+        user_input=str(input(" kindly enter either of below option \n  1.exit ==> to finish the instance id input \n 2. any input ==> to continue the instance id input\n"))
+        if user_input == "exit":
+            flag=False
+
+
+
+
+#get the meta data for the instances
+get_instance_metadata(input_region_name,input_instance_list)
+
